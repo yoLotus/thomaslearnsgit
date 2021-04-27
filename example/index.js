0a67cb861021a6c6ba733211af2616c95036b852
etienne <etienne@airfair.io>
2021-04-27 22:48:18 +0200

    a message

--- example/index.js
+++ example/index.js
@@ -1,13 +1,17 @@
-const myFonction = () => {
+const myFunction = () => {
   let a = 3;
 
   if (a > 5) {
     console.log(a);
   } else {
-    console.log('too small');
+    console.log('very small');
   }
 }
 
 const anotherAwesomeFunction = () => {
   console.log('awesome!');
+}
+
+const sum = (a, b) =>  {
+  return a + b;
 }
